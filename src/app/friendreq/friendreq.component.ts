37cab0e511de13f507d345bc626cbea8de2e9c78
Abdul Kyume <abbirtak3@gmail.com>
2022-11-17 14:34:57 +0600

    as

--- src/app/friendreq/friendreq.component.ts
+++ src/app/friendreq/friendreq.component.ts
@@ -4,34 +4,60 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 @Component({
   selector: 'app-friendreq',
   templateUrl: './friendreq.component.html',
-  styleUrls: ['./friendreq.component.css']
+  styleUrls: ['./friendreq.component.css'],
 })
 export class FriendreqComponent implements OnInit {
   userinfo: any;
   ausers: any;
   users!: any;
-  constructor(private afs: AngularFirestore) { }
+  constructor(private afs: AngularFirestore) {}
 
   ngOnInit(): void {
     this.userinfo = JSON.parse(localStorage.getItem('user')!);
-    this.afs.collection('addfriends').valueChanges().subscribe(userss => {
-      this.ausers = userss;
-      this.ausers = this.ausers.filter((user: any) => user.uid == this.userinfo.uid);
-      var freq: any = [];
-      for (var i = 0; i < this.ausers.length; i++) {
-        freq.push(this.ausers[i].afduid);
-      }
-      this.afs.collection('users', ref => ref.where('uid', 'in', freq)).valueChanges().subscribe(ussers => {
-        this.users = ussers;
-      })
-    });
+    this.afs
+      .collection('addfriends')
+      .valueChanges()
+      .subscribe((userss) => {
+        this.ausers = userss;
+        this.ausers = this.ausers.filter(
+          (user: any) => user.uid == this.userinfo.uid
+        );
+        var freq: any = [];
+        for (var i = 0; i < this.ausers.length; i++) {
+          freq.push(this.ausers[i].afduid);
+        }
+        if(freq.length>0){
+          this.afs
+          .collection('users', (ref) => ref.where('uid', 'in', freq))
+          .valueChanges()
+          .subscribe((ussers) => {
+            this.users = ussers;
+          });
+        }
+        
+      });
   }
 
   delfriendreq(afdid: any) {
     this.userinfo = JSON.parse(localStorage.getItem('user')!);
-    this.afs.doc(`addfriends/${afdid}`).delete();
+    this.afs
+      .collection('addfriends')
+      .valueChanges()
+      .subscribe((ussers) => {
+        this.users = ussers;
+        this.users = this.users.filter(
+          (user: any) => user.afduid == afdid
+        );
+        console.log(this.users)
+      });
+    // this.afs
+    //   .collection('addfriends')
+    //   .doc(`${this.userinfo.uid}`)
+    //   .delete()
+    //   .then(() => console.log('Item deleted'))
+    //   .catch((err) => console.log('Error!', err));
   }
-  accfriendreq(afdid:any){
-    
+  accfriendreq(afdid: any) {
+    console.log(afdid);
   }
 }
