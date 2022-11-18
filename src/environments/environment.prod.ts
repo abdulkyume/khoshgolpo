6ef3a12b1d6feb81653d304d28eca92e1c0090d5
Abdul Kyume <abbirtak3@gmail.com>
2022-11-18 20:14:52 +0600

    asd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBNAXd8KYQViv25_gmDwLGhQ8ETBSsHsqY',
+
+    authDomain: 'khoshgolpo-77921.firebaseapp.com',
+
+    databaseURL:
+      'https://khoshgolpo-77921-default-rtdb.asia-southeast1.firebasedatabase.app',
+
+    projectId: 'khoshgolpo-77921',
+
+    storageBucket: 'khoshgolpo-77921.appspot.com',
+
+    messagingSenderId: '156996442514',
+
+    appId: '1:156996442514:web:d4a5b27b1f7965ce26c94b',
+
+    measurementId: 'G-QEZ4JTYGKZ',
+  },
 };
